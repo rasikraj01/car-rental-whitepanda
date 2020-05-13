c27a0bb2bb836983d29ea3dbb21820ccfeea776d
rasikraj00 <rasikraj01@gmail.com>
2020-05-14 03:13:34 +0530

    path update

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ app.use('/api/', bookingRoutes);
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.resolve(__dirname, '/client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
   
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-      res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
 
